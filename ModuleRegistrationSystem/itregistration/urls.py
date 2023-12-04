4f834b1bfa6482a0e266285585470cd426b8e63e
C1026327 <c1026327@my.shu.ac.uk>
2023-12-04 17:46:24 +0000

    django initial setup

---
+++ ModuleRegistrationSystem/itregistration/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'itregistration'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('about', views.about, name='contact'),
+]
